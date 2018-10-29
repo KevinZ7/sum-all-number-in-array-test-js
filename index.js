f9db629442d5bb5af21b374618334f9ea078b565
KevinZ7 <kevinz.z.zhou@gmail.com>
2018-10-29 05:03:02 +0000

    finished recursive excercice

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+
+  array.forEach((number) => {
+    if(Array.isArray(number)) {
+      sum += sumItems(number);
+    } else {
+      sum += number;
+    }
+  })
+
+  return sum;
+
 }
 
 module.exports = sumItems;
